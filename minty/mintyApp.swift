f8f8900adc37097d4a2c3deff9ab98ced1271720
jebeene <89660618+jebeene@users.noreply.github.com>
2024-01-11 16:55:47 -0500

    Initial Commit

---
+++ minty/mintyApp.swift
@@ -0,0 +1,17 @@
+//
+//  mintyApp.swift
+//  minty
+//
+//  Created by Jacob Beene on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct mintyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
